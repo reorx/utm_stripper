2541e2f4e0eb88e14731134738dc8c92c38cea53
reorx <novoreorx@gmail.com>
2015-10-31 14:32:07 +0800

    try different mechanisms on deciding when to show page action

--- utm_stripper/background.js
+++ utm_stripper/background.js
@@ -1,48 +1,113 @@
 console.log('load background.js');
 
 // NOTE set to false in production
-var DEBUG = true;
-
-
-var log = function() {
-    if (DEBUG)
-        console.log.apply(console, arguments);
-};
+var DEBUG = true,
+    log = function() {
+        if (DEBUG)
+            console.log.apply(console, arguments);
+    },
+    log_error = function() {
+        console.error.apply(console, arguments);
+    },
+    // Recycle on tab close
+    tab_states = {},
+    // setTabState = function(tabId, last_url_noquery, query) {
+    //     tab_states[tabId] = {
+    //         last_url_noquery: last_url_noquery,
+    //         last_query: query
+    //     };
+    // },
+    getTabState = function(tabId) {
+        if (!(tabId in tab_states)) {
+            tab_states[tabId] = {
+                // popup_indicator is the pridiction of future, what the action
+                // will be for the next time and further, it has no assocication
+                // with the current status.
+                popup_indicator: 0
+            };
+        }
+        return tab_states[tabId];
+    },
+    deleteTabState = function(tabId) {
+        delete tab_states[tabId];
+    };
 
 
 var onURLChanged = function(event, o) {
     var url = o.url,
         tabId = o.id,
+        st = getTabState(tabId),
         stripped;
     log('URL changed', event, o);
+
+    // if (st.popup_indicator < 2)
+    //     st.popup_indicator = 0;
+    // else
+    //     st.popup_indicator -= 1;
 
     var parsed = parseURL(url);
     log('url noquery:', parsed.url_noquery,
         '\nquery:', parsed.query,
-        '\nfragment:', parsed.fragment);
+        '\nfragment:', parsed.fragment,
+        '\npopup_indicator:', st.popup_indicator);
 
-    if (parsed.query)
-        chrome.pageAction.show(tabId);
+    if (parsed.query) {
+        // As long as there's query, the page action button should be shown
 
-    /* TODO popup when click the second time, with more options like:
-    - complete strip
-    - configure rules
+        if (st.popup_indicator > 0) {
+            setActionStatus(tabId, 'visiable_popup');
+        } else {
+            setActionStatus(tabId, 'visible_click');
+        }
 
-    if (url.indexOf('baidu') > -1) {
-        chrome.pageAction.setPopup({
-            tabId: tabId,
-            popup: 'popup.html'
-        });
+        // if (st.last_url_noquery == parsed.url_noquery) {
+        //     // Within the same
+        //     if (st.query) {
+        //         // If there's query before, show popup to give more options for page action button
+        //         setActionStatus(tabId, 'visiable_popup');
+        //     } else {
+        //         // If no query before, this time could still have a one click chance
+        //         setActionStatus(tabId, 'visible_click');
+        //     }
+        // } else {
+        //     // This means we open a new url, either from new tab or previous url,
+        //     // give a chance to strip by one click
+        //     setActionStatus(tabId, 'visible_click');
+        // }
     } else {
-        chrome.pageAction.setPopup({
-            tabId: tabId,
-            popup: ''
-        });
-        chrome.pageAction.onClicked.addListener(function() {
-            log('clicked page action');
-        });
+        // If no query, simpliy hide the page action button
+        setActionStatus(tabId, 'invisible');
     }
-    */
+    st.popup_indicator = 0;
+};
+
+
+var setActionStatus = function(tabId, st) {
+    console.log('setActionStatus', tabId, st);
+    // visible_click, visiable_popup, invisible
+    switch (st) {
+        case 'visible_click':
+            chrome.pageAction.setPopup({
+                tabId: tabId,
+                popup: ''
+            });
+            chrome.pageAction.show(tabId);
+            break;
+        case 'visiable_popup':
+            // When popup is set, pageAction.onClicked won't be triggered
+            chrome.pageAction.setPopup({
+                tabId: tabId,
+                popup: 'popup.html'
+            });
+            chrome.pageAction.show(tabId);
+            break;
+        case 'invisible':
+            chrome.pageAction.hide(tabId);
+            break;
+        default:
+            log_error('Bad status in setActionStatus:', st);
+            break;
+    }
 };
 
 
@@ -98,6 +163,7 @@ var parseURL = function(url) {
 };
 
 
+// idempotent !!!
 var stripURL = function(url) {
     // TODO avoid multiple calling of parseURL
     var parsed = parseURL(url),
@@ -133,13 +199,25 @@ var stripURL = function(url) {
 
 // Listen on click
 chrome.pageAction.onClicked.addListener(function(o) {
-    var url = o.url;
-    stripped = stripURL(url);
+    var tabId = o.id,
+        url = o.url,
+        stripped = stripURL(url);
+    console.log('click', tabId, arguments);
+
+    var st = getTabState(tabId);
 
     if (url != stripped) {
-        chrome.tabs.update(o.tabId, {
+        // 2 means only next time (url changes) it would be popup, after next time it won't be
+        // st.popup_indicator = 2;
+        st.popup_indicator = 1;
+        chrome.tabs.update(tabId, {
             url: stripped
         });
+    } else {
+        // 1 means next time (url changes) it won't be popup, whereas 0 is the initial
+        // st.popup_indicator = 1;
+        st.popup_indicator = 0;
+        setActionStatus(tabId, 'visiable_popup');
     }
     console.log('stripped:', stripped);
 });
@@ -147,6 +225,12 @@ chrome.pageAction.onClicked.addListener(function(o) {
 
 // Listen on url change
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.status == 'loading')
+    if (changeInfo.status == 'loading' &&
+        (tab.url.indexOf('http:') == 0 || tab.url.indexOf('https:') == 0))
         onURLChanged('tabs.onUpdated', tab);
+});
+
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    log('tab removed', tabId);
 });
